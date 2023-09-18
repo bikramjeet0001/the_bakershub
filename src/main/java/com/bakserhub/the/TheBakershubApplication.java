22bdce0e050f47c768499375b3513589f9651b52
Bikramjeet Singh <bikramjeet.sarbjeet@sifycorp.com>
2023-09-18 13:54:45 +0530

    Added initial spring boot project
    Added initial spring boot project

---
+++ src/main/java/com/bakserhub/the/TheBakershubApplication.java
@@ -0,0 +1,13 @@
+package com.bakserhub.the;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBakershubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBakershubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bakserhub/the/TheBakershubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bakserhub.the;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheBakershubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
